847fcf17ce18ef30ad45d3b9c9e82c6c0ea743f9
alk3mist <alexandr.gavrilin@hotmail.com>
2019-04-16 18:12:44 +0300

    Add migrate to Dockerfile

---
+++ backend/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py migrate
+
+# Start server
+echo "Starting server"
+python manage.py runserver 0.0.0.0:80
